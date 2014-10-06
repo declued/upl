20a8f448e1ebea2e655ed5ff4bbb8d4047a48d27
yzt <yaserzt@gmail.com>
2014-10-06 07:29:18 +0330

    Wrote a lexer interface (and bare skeleton implementation.)

--- include/upl/lexer.hpp
+++ include/upl/lexer.hpp
@@ -3,6 +3,9 @@
 //======================================================================
 
 #include <upl/common.hpp>
+#include <upl/errors.hpp>
+#include <upl/input.hpp>
+#include <upl/tokens.hpp>
 
 //======================================================================
 
@@ -12,7 +15,27 @@ namespace UPL {
 //----------------------------------------------------------------------
 //======================================================================
 
-}	// namespace UPL
+class Lexer
+{
+public:
+	explicit Lexer (InputStream & input, Error::Reporter & reporter);
+
+	bool eoi () const {return m_cur_tok.is(TT::EOI);}
+	bool error () const {return m_has_error;}
+
+	Token const & curr () const {return m_cur_tok;}
+
+	bool pop ();
+
+private:
+	InputStream & m_input;
+	Error::Reporter & m_reporter;
+	Token m_cur_tok;
+	bool m_has_error;
+};
 
 //======================================================================
 
+}	// namespace UPL
+
+//======================================================================
