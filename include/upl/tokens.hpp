ee6595f664c00d90bdde25c609bade057bedfe9b
yzt <yaserzt@gmail.com>
2014-10-09 03:21:21 +0330

    Implemented some missing ctors in UPL::Token.

--- include/upl/tokens.hpp
+++ include/upl/tokens.hpp
@@ -33,11 +33,20 @@ class Token
 public:
 	Token () : m_type (TT::Empty) {}
 	Token (TT type, Location const & location) : m_type (type), m_loc (location) {}
-	Token (TT type, Location const & location, String uncooked_value);
-	Token (TT type, Location const & location, String uncooked_value, Bool bool_value);
-	Token (TT type, Location const & location, String uncooked_value, Int int_value);
-	Token (TT type, Location const & location, String uncooked_value, Real real_value);
-	Token (TT type, Location const & location, String uncooked_value, String string_value);
+	Token (TT type, Location const & location, String uncooked_value)
+		: m_type (type), m_loc (location), m_uncooked (std::move(uncooked_value)) {}
+	Token (TT type, Location const & location, String uncooked_value, Bool bool_value)
+		: m_type (type), m_loc (location), m_uncooked (std::move(uncooked_value))
+	{m_value.b = bool_value;}
+	Token (TT type, Location const & location, String uncooked_value, Int int_value)
+		: m_type (type), m_loc (location), m_uncooked (std::move(uncooked_value))
+	{m_value.i = int_value;}
+	Token (TT type, Location const & location, String uncooked_value, Real real_value)
+		: m_type (type), m_loc (location), m_uncooked (std::move(uncooked_value))
+	{m_value.r = real_value;}
+	Token (TT type, Location const & location, String uncooked_value, String string_value)
+		: m_type (type), m_loc (location), m_uncooked (std::move(uncooked_value))
+	{m_value.s = std::move(string_value);}
 
 	bool is (TT tok_type) const {return m_type == tok_type;}
 	bool isnt (TT tok_type) const {return m_type != tok_type;}
@@ -59,9 +68,9 @@ private:
 	// A better (smaller) alternative is Boost.Variant instead of struct
 	struct {
 		String s;
-		Real r;
-		Int i;
-		Bool b;
+		Real r = 0;
+		Int i = 0;
+		Bool b = false;
 	} m_value;
 };
 
