d7fa5733d8abbd8b367a4c3500c1619037adf208
yzt <yaserzt@gmail.com>
2014-10-09 05:04:00 +0330

    Added a simple test for lexer to the Playpen application.
    This is NOT a unit test or anything. Just a function to call the lexer on a sample input (in the file "docs/sample-program-00.upl" which you must copy to the directory where you run the playpen from.)

--- src/playpen/upl_playpen_main.cpp
+++ src/playpen/upl_playpen_main.cpp
@@ -1,8 +1,9 @@
 //======================================================================
 
-#include <upl/common.hpp>
-#include <upl/errors.hpp>
+#include <upl/lexer.hpp>
 #include <upl/input.hpp>
+#include <upl/errors.hpp>
+#include <upl/common.hpp>
 
 #include <cstdint>
 #include <iostream>
@@ -12,8 +13,10 @@
 //----------------------------------------------------------------------
 //======================================================================
 
+void ReportErrors (UPL::Error::Reporter const & err);
 void TestStringConversions ();
 void TestInputStream ();
+void TestLexer ();
 
 //======================================================================
 
@@ -22,19 +25,47 @@ int main (int /*argc*/, char * /*argv*/[])
 	std::cout << "This is the UPL playpen, where testing and tinkering happens.\n";
 	std::cout << std::endl;
 
-	std::cout << "Testing string conversions: " << std::endl;
+	std::cout << "==========================" << std::endl;
+	std::cout << "Testing string conversions" << std::endl;
+	std::cout << "--------------------------" << std::endl;
 	TestStringConversions ();
 	std::cout << std::endl;
 
-	std::cout << "Testing input streams: " << std::endl;
+	std::cout << "=====================" << std::endl;
+	std::cout << "Testing input streams" << std::endl;
+	std::cout << "---------------------" << std::endl;
 	TestInputStream ();
 	std::cout << std::endl;
 
+	std::cout << "=================" << std::endl;
+	std::cout << "Testing the lexer" << std::endl;
+	std::cout << "-----------------" << std::endl;
+	TestLexer ();
+	std::cout << std::endl;
 
 	return 0;
 }
 
 //======================================================================
+
+void ReportErrors (UPL::Error::Reporter const & err)
+{
+	using std::wcout;
+	using std::cout;
+	using std::endl;
+
+	wcout << "Errors: " << err.count() << endl;
+	for (auto const & er : err.reports())
+		wcout
+			<< "  "
+			<< UPL::ToString(er.file()) << ":" << er.location().line() << "," << er.location().column()
+			<< " (" << int(er.category()) << "," << int(er.severity())
+			<< ") : (" << er.number() << ") "
+			<< er.message()
+			<< endl;
+}
+
+//----------------------------------------------------------------------
 
 void TestStringConversions ()
 {
@@ -66,23 +97,41 @@ void TestInputStream ()
 	UPL::Error::Reporter err;
 	UPL::UTF8FileStream inp ("sample-utf8-input.txt", err);
 
-	do {
-		wcout << ((inp.curr() < 32 || inp.curr() > 255) ? L'?' : inp.curr());
-		wcout << " (0x" << hex << int(inp.curr()) << dec << ")";
-		wcout << " @" << inp.location().line() << "," << inp.location().column();
-		wcout << endl;
-	} while (inp.pop());
+	if (!inp.eoi() && !inp.error())
+		do {
+			wcout << ((inp.curr() < 32 || inp.curr() > 255) ? L'?' : inp.curr());
+			wcout << " (0x" << hex << int(inp.curr()) << dec << ")";
+			wcout << " @" << inp.location().line() << "," << inp.location().column();
+			wcout << endl;
+		} while (inp.pop());
 
 	wcout << endl;
-	wcout << "Errors: " << err.count() << endl;
-	for (auto const & er : err.reports())
-		wcout
-			<< "  "
-			<< UPL::ToString(er.file()) << ":" << er.location().line() << "," << er.location().column()
-			<< " (" << int(er.category()) << "," << int(er.severity())
-			<< ") : (" << er.number() << ") "
-			<< er.message()
-			<< endl;
+	ReportErrors (err);
+	wcout << endl;
+}
+
+//----------------------------------------------------------------------
+
+void TestLexer ()
+{
+	using std::wcout;
+	using std::endl;
+
+	UPL::Error::Reporter err;
+	UPL::UTF8FileStream inp ("sample-program-00.upl", err);
+	UPL::Lexer lex (inp, err);
+
+	if (!lex.eoi() && !lex.error())
+		do {
+			UPL::Token const & t = lex.curr();
+			wcout
+				<< "@" << t.location().line() << "," << t.location().column()
+				<< " : " << t.typeStr() << " (" << t.uncookedValue() << ")"
+				<< endl;
+		} while (lex.pop());
+
+	wcout << endl;
+	ReportErrors (err);
 	wcout << endl;
 }
 
