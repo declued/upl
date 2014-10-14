2bc08f5bd648f84f111106541a816e7222c15296
yzt <yaserzt@gmail.com>
2014-10-14 22:36:11 +0330

    Created a skeleton for the parser and the AST hierarchy.
    Each non-literal in our grammar needs a corresponding AST class (at least.) These all go into "ast.hpp" (and some into "ast_details.hpp".)
    
    Also added a pair of empty files for the type system ("types.hpp" and "types.cpp".)
    
    Updated the "CMakeLists.txt" file to include the new files and to set the warning level higher ("/W4" on MSVC and "-Wall" on GCC. TODO: Add clang support!

---
+++ include/upl/ast_details.hpp
@@ -0,0 +1,96 @@
+#pragma once
+
+//======================================================================
+
+#include <upl/common.hpp>
+#include <upl/definitions.hpp>
+
+#include <memory>
+
+//======================================================================
+
+namespace UPL {
+	namespace AST {
+
+//======================================================================
+/* These are the helper "Abstract Syntax Tree" node types. */
+//----------------------------------------------------------------------
+
+class Node
+{
+public:
+	virtual ~Node () {}
+	virtual Node * clone () const = 0;
+	virtual String printable (int depth = 0) const = 0;
+
+	/* more convenient casting */
+	template <typename T>
+	T * as ()
+	{
+		auto ret = dynamic_cast<T *>(this);
+		assert (nullptr != ret);
+		return ret;
+	}
+
+	template <typename T>
+	T const * as () const
+	{
+		auto ret = dynamic_cast<T const *>(this);
+		assert (nullptr != ret);
+		return ret;
+	}
+};
+
+//----------------------------------------------------------------------
+
+class Parent : public Node
+{
+public:
+	typedef std::vector<std::unique_ptr<Node>> ChildContainer;
+
+public:
+	virtual ~Parent () {}
+	virtual Node * clone () const = 0;
+
+	ChildContainer & children () {return m_children;}
+	ChildContainer const & children () const {return m_children;}
+
+private:
+	ChildContainer m_children;
+};
+
+//----------------------------------------------------------------------
+
+class Value;
+
+// I don't know what to do about the "type" of an expression exactly.
+// For now, THIS IS JUST A PLACEHOLDER.
+class Type {};
+
+class ExprBase : public Parent
+{
+public:
+	virtual ~ExprBase () {}
+	virtual Node * clone () const = 0;
+
+	virtual Type evalType () const = 0;
+	virtual Value eval () const = 0;
+};
+
+//----------------------------------------------------------------------
+
+class Value : public ExprBase
+{
+public:
+	virtual ~Value () {}
+	virtual Node * clone () const = 0;
+};
+//======================================================================
+//----------------------------------------------------------------------
+//======================================================================
+
+	}	// namespace AST
+}	// namespace UPL
+
+//======================================================================
+
--- include/upl/parser.hpp
+++ include/upl/parser.hpp
@@ -3,10 +3,42 @@
 //======================================================================
 
 #include <upl/common.hpp>
+#include <upl/errors.hpp>
+#include <upl/lexer.hpp>
+#include <upl/ast.hpp>
 
 //======================================================================
 
 namespace UPL {
+
+//======================================================================
+
+class Parser
+{
+public:
+	Parser (Lexer & lexer, Error::Reporter & reporter);
+
+	// Non-copyable and non-movable (for now.)
+	Parser (Parser const &) = delete;
+	Parser (Parser &&) = delete;
+	Parser & operator = (Parser const &) = delete;
+	Parser & operator = (Parser &&) = delete;
+
+	AST::Node * parse ();
+
+private:
+	AST::Program * parseProgram ();
+	AST::Statement * parseStatement ();
+	AST::Declaration * parseDeclaration ();
+	AST::Expression * parseExpression ();
+	// .
+	// .
+	// .
+
+private:
+	Lexer & m_lexer;
+	Error::Reporter & m_reporter;
+};
 
 //======================================================================
 //----------------------------------------------------------------------
@@ -15,4 +47,3 @@ namespace UPL {
 }	// namespace UPL
 
 //======================================================================
-
